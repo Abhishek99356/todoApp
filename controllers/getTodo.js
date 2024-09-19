797254e91432b3ed022d6485ce0851e609d05f01
Abhishek99356 <abhishekyadav40854@gmail.comgit config --global user.email>
2024-09-20 02:30:24 +0530

    add code

--- controllers/getTodo.js
+++ controllers/getTodo.js
@@ -6,8 +6,57 @@ const Todo = require("../models/Todo");
 exports.getTodo = async(req, res) => {
     try{
         //fetch all todo items from database
+        const todos = await Todo.find({});
+
+        //responce
+        res.status(200)
+        .json({
+            success:true,
+            data:todos,
+            message: "Entire Todo Data is fetched",
+        })
     }
     catch(err){
+        console.error(err);
+        res.status(500)
+        .json({
+            success:false,
+            error:err.message,
+            message:"server Error"
+          });
         
+    }
+}
+
+
+
+exports.getTodoById = async(req,res) => {
+    try{
+       //extract todo items basis on id
+       const id = req.params.id;
+       const todo = await Todo.findById({_id: id})
+
+       //data forgiven id not found
+       if(!todo){
+        return res.status(400).json({
+            success:false,
+            message:"No Data Found woth given id",
+        })
+       }
+       //data for given id found
+       res.status(200).json({
+        success:true,
+        data:todo,
+        message:`Todo &{id} data success full fetched`,
+       })
+    }
+    catch(err){
+        console.error(err);
+        res.status(500)
+        .json({
+            success:false,
+            error:err.message,
+            message:"server Error"
+          });
     }
 }
